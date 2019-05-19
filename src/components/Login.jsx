4e1f59b266e8ca3b8c97fcd5a52c8357463ff4c7
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2019-05-20 02:53:29 +0600

    Add dummy login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,8 +5,39 @@ class Login extends PureComponent {
   render() {
     return (
       <div>
-        <div>Login Component</div>
-        <Link to="/register">Registration</Link><br/>
+        <div className="jumbotron">
+          <div className="container">
+            <h1 className="display-4">Login</h1>
+          </div>
+        </div>
+
+        <form>
+          <div className="form-group">
+            <label for="username">Username</label>
+            <input
+              type="text"
+              className="form-control"
+              id="username"
+              placeholder="Username"
+            />
+          </div>
+
+          <div className="form-group">
+            <label for="password">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Password"
+            />
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+        <Link to="/register">Registration</Link>
+        <br />
         <Link to="/users">Users</Link>
       </div>
     );
